d053443d95dc8b5bd7169891f698192246ce00f2
A Oladipupo-Usoro <aolausoro@gmail.com>
2021-02-26 22:01:11 +0000

    modified: postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,34 @@
 const tailwindcss = require("tailwindcss");
 module.exports = {
-  plugins: [tailwindcss("./tailwind.js"), require("autoprefixer")],
+  plugins: [
+    tailwindcss('./tailwind.js'),
+    require('autoprefixer'),
+    [
+      '@fullhuman/postcss-purgecss',
+      process.env.NODE_ENV === 'production'
+        ? {
+            // the paths to all template files
+            content: [
+              './pages/**/*.{js,jsx,ts,tsx}',
+              './components/**/*.{js,jsx,ts,tsx}',
+            ],
+            // function used to extract class names from the templates
+            defaultExtractor: (content) =>
+              content.match(/[\w-/:]+(?<!:)/g) || [],
+          }
+        : false,
+    ],
+    [
+      'postcss-preset-env',
+      {
+        autoprefixer: {
+          flexbox: 'no-2009',
+        },
+        stage: 3,
+        features: {
+          'custom-properties': false,
+        },
+      },
+    ],
+  ],
 };
